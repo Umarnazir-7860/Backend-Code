20a0a69f3cb4f854e850eace98b8d1bc8ce0ffca
Umarnazir-7860 <nazirumar@7860.gmail.com>
2025-03-27 17:58:48 +0500

    correct stack spellings

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -2,15 +2,15 @@
       constructor(statusCode,
          message="Internal Server Error",
          errors=[],
-        statck="" ){	
+        stack="" ){	
         super(message);
         this.statusCode = statusCode;
         this.message = message;
         this.errors = errors;
         this.data = null;
         this.success = false;
-    if(statck){
-        this.statck = statck;
+    if(stack){
+        this.stack = stack;
     }else{
         Error.captureStackTrace(this, this.constructor);    }  
     }
